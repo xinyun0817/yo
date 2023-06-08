74caf581982de14c989ba114e99b046dd2ba33e8
XIN YUN <xinyun1994@hotmail.com>
2023-06-08 15:09:47 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  Card,
+  CardBody,
+  Table
+} from 'reactstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SystemSettings extends Component {
+
+  componentDidMount() {
+    document.body.style.background = 'linear-gradient(#e66465, #9198e5) fixed'
+  }
+
+  render = () => {
+    const students = [
+      {
+        name: 'Ang',
+        age: '18',
+        gender: 'Female'
+      },
+      {
+        name: 'Tan',
+        age: '18',
+        gender: 'Male'
+      },
+      {
+        name: 'Yeoh',
+        age: '17',
+        gender: 'Female'
+      }
+    ]
+    return (
+      <>
+        <h1>Student list</h1>
+        <div className='m-4'>
+          <Table striped className='p-4'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                students.map( student => {
+                  return(
+                  
+                    
+                        <tr>
+                          <td>{ student.name }</td>
+                          <td>{ student.age }</td>
+                          <td>{ student.gender }</td>
+                        </tr>
+                    
+                  )
+                })
+              }
+            </tbody>
+          </Table>
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default SystemSettings
